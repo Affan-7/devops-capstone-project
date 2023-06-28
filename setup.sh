e0d88b61d644d97f3ccb9674eb4272dbd1efdff8
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-06-28 19:18:11 +0530

    Added username and email in git config local variables

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 export GITHUB_ACCOUNT=Affan-7
 bash ./bin/setup.sh
 pip install -r requirements.txt
+git config --local user.name "Mohammed Affan"
+git config --local user.email mohammed.affan.727@gmail.com
