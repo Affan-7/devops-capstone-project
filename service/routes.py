0c667419461abb174abbee300e50c97eaa32f0f1
Mohammed Affan <mohammed.affan.727@gmail.com>
2023-07-01 07:06:08 -0400

    Added linting fixes

--- service/routes.py
+++ service/routes.py
@@ -61,8 +61,9 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
-def list_accounts():    
+def list_accounts():
     """
     Lists all accounts
     This endpoint will list all accounts available in the database
@@ -71,7 +72,7 @@ def list_accounts():
 
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    
+
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -81,13 +82,13 @@ def list_accounts():
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
-def read_account(account_id):    
+def read_account(account_id):
     """
     Reads an Account
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
@@ -97,6 +98,7 @@ def read_account(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_account(account_id):
